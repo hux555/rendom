8893ca4bd453a6471aa978a8b8b7b2ad622a0fa3
hux555 <119776008+hux555@users.noreply.github.com>
2023-07-27 18:00:24 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -5,9 +5,9 @@ const net = require('net');
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+//let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
-
-let address = '';
+let address = 'cc6.jlin52.eu.org';
 let portWithRandomLog = '';
 const log = (/** @type {string} */ info, /** @type {string | undefined} */ event) => {
     console.log(`[${address}:${portWithRandomLog}] ${info}`, event || '');
